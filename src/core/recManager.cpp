5510d94c757825c26278f347dbea2290325dec05
gvnnz <monocasual@gmail.com>
2021-04-16 21:34:11 +0200

    [WIP] Refresh Main Window when free rec ends

--- src/core/recManager.cpp
+++ src/core/recManager.cpp
@@ -232,6 +232,7 @@ void stopInputRec(InputRecMode recMode)
 		clock::rewind();
 		clock::setBpm(clock::calcBpmFromRec(recordedFrames));
 		mixer::setEndOfRecCallback(nullptr);
+		model::triggerSwapCb(model::SwapType::HARD);
 	}
 }
 
